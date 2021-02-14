b3d15b3bb509369596c13acb08c05a2b94d5caa4
wxcv-ai <78027438+wxcv-ai@users.noreply.github.com>
2021-02-14 18:05:08 +0100

    Update all_pairs.py

--- all_pairs.py
+++ all_pairs.py
@@ -3,43 +3,46 @@ import requests
 import asyncpg 
 import asyncio
 import subprocess
-def pairs():
-	base_url = "https://bitmax.io"
-	quote_url = "/api/pro/v1/products"
-	url = base_url+quote_url
-	response = requests.request("GET", url)
-	message = json.loads(response.text)
-	final_message = message['data']
-	pairs = []
-	for symbol in final_message:
-		symbol = symbol["symbol"]
-		pairs.append(symbol)
-	return pairs
+class assets :
+	def pairs(self):
+		self.base_url = "https://bitmax.io"
+		self.quote_url = "/api/pro/v1/products"
+		self.url = base_url+quote_url
+		self.response = requests.request("GET", self.url)
+		self.message = json.loads(self.response.text)
+		self.final_message = message['data']
+		pairs = []
+		for symbol in final_message:
+			self.symbol = symbol["symbol"]
+			pairs.append(self.symbol)
+		return pairs
 
-pairs()
+	
 
-async def get_pairs():
-	base_url = "https://bitmax.io"
-	quote_url = "/api/pro/v1/products"
-	url = base_url+quote_url
-	response = requests.request("GET", url)
-	message = json.loads(response.text)
-	final_message = message['data']
-	pairs = []
-	exchange = 'bitmax'
-	dsn = "postgres://postgres:kirahavethedeathnote123A@localhost:5432/"+exchange
-	print('inserting pairs is in process ...')
-	for symbol in final_message:
-		symbol = symbol["symbol"]
-		pairs.append(symbol)
-		conn = await asyncpg.connect(dsn)
-		INSERT_INTO_TABLE= 'INSERT INTO all_pairs (pair) VALUES ' +"("+"'"+str(symbol)+"'"+")"
-		await conn.execute(INSERT_INTO_TABLE)
-		await conn.close()
-	print('all pairs has been inserted successfully')
-	return pairs
+	async def get_pairs(self):
+		self.base_url = "https://bitmax.io"
+		self.quote_url = "/api/pro/v1/products"
+		self.url = base_url+quote_url
+		self.response = requests.request("GET", self.url)
+		self.message = json.loads(self.response.text)
+		self.final_message = message['data']
+		pairs = []
+		self.exchange = 'bitmax'
+		self.dsn = "postgres://postgres:kirahavethedeathnote123A@localhost:5432/"+exchange
+		print('inserting pairs is in process ...')
+		for symbol in self.final_message:
+			self.symbol = symbol["symbol"]
+			pairs.appendself.(symbol)
+			self.conn = await asyncpg.connect(self.dsn)
+			self.INSERT_INTO_TABLE= 'INSERT INTO all_pairs (pair) VALUES ' +"("+"'"+str(symbol)+"'"+")"
+			await conn.execute(self.INSERT_INTO_TABLE)
+			await conn.close()
+		print('all pairs has been inserted successfully')
+		return pairs
 
-asyncio.get_event_loop().run_until_complete(get_pairs())
+	
+assets().pairs()
+asyncio.get_event_loop().run_until_complete(assets().get_pairs())
 
 
 
