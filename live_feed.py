dd319a8535916d39d864fb8ed3d5c3601a61df98
wxcv-ai <78027438+wxcv-ai@users.noreply.github.com>
2021-02-20 23:48:34 +0100

    Update live_feed.py

--- live_feed.py
+++ live_feed.py
@@ -16,20 +16,20 @@ class connection:
 		self.response =  ws.recv()
 		self.msg = json.loads(self.response)
 		#print(msg)
-		if msg['m'] == 'bbo':
-			self.lowest_ask = msg['data']['ask']
-			self.highest_bid = msg['data']['bid']
+		if self.msg['m'] == 'bbo':
+			self.lowest_ask = self.msg['data']['ask']
+			self.highest_bid = self.msg['data']['bid']
 			self.order_book={}
-			self.order_book['ask'] = lowest_ask
-			self.order_book['bid'] = highest_bid
+			self.order_book['ask'] = self.lowest_ask
+			self.order_book['bid'] = self.highest_bid
 			#DSN = "postgres://postgres:kirahavethedeathnote123A@localhost:5432/bitmax"
 			#conn = await asyncpg.connect(DSN)
 			#UPDATE_TABLE = 'UPDATE live_feed SET live_feed ='  +"'"+str(order_book).replace("'",'"')+ "'"+' WHERE pair =  ' + "'"+str(pair)+"'"
 			#print(UPDATE_TABLE)
 			#await conn.execute(UPDATE_TABLE)
 			#await conn.close()
-			#print(msg["symbol"])
-		if msg['m'] == 'ping':
+			print(self.msg["symbol"])
+		if self.msg['m'] == 'ping':
 
 			self.ping_msg = {"op": "pong"}
 			ws.send(json.dumps(self.ping_msg))
@@ -39,7 +39,7 @@ class connection:
 	def initiations(self,pair):
 
 		self.wbs_url = "wss://bitmax.io/1/api/pro/v1/stream"
-		self.ws = create_connection(wbs_url)
+		self.ws = create_connection(self.wbs_url)
 		self.subscribe_msg= { "op": "sub", "id": str(pair) ,"ch":"bbo:"+pair+""}
 		self.ws.send(json.dumps(self.subscribe_msg))	
 		
@@ -47,7 +47,7 @@ class connection:
 		while self.connected:
 			try:
 				
-				self.response = ws.recv()
+				self.response = self.ws.recv()
 				self.msg = json.loads(self.response)
 
 				connection().handle_msg(self.msg,self.ws)
@@ -57,26 +57,29 @@ class connection:
 				break
 		return 0
 
-	def start (self ,reloaded_pairs,pairs) :
+	def start (self,reloaded_pairs,pairs) :
 		if len(reloaded_pairs) == 0:
 			for pair in pairs:
 				print(pair)
 
-				self.p = multiprocessing.Process(target=connection().initiations, args= (pair,))
-				self.freeze_support()
-				self.self.os.fork()
-				self.p.start()
+				p = multiprocessing.Process(target=connection().initiations, args=(pair,))
+				freeze_support()
+				os.fork()
+				freeze_support()
+				os.fork()
+				p.start()
 				
 				
 
 		else:
 			for pair in reloaded_pairs:
-				self.p = multiprocessing.Process(target=connection().initiations, args= (pair,))
-				self.freeze_support()
-				self.os.fork()
-				self.p.start()
-				self.freeze_support()
-				self.reloaded_pairs.pop(pair)
+				p = multiprocessing.Process(target=connection().initiations, args= (pair,))
+				freeze_support()
+				os.fork()
+				freeze_support()
+				p.start()
+
+				reloaded_pairs.pop(pair)
 
 
 
