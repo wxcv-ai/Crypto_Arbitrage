be3021bdc32326a54818350a36fdbbb471c67bca
wxcv-ai <78027438+wxcv-ai@users.noreply.github.com>
2021-02-20 17:12:54 +0100

    Update all_pairs.py

--- all_pairs.py
+++ all_pairs.py
@@ -7,12 +7,12 @@ class assets :
 	def pairs(self):
 		self.base_url = "https://bitmax.io"
 		self.quote_url = "/api/pro/v1/products"
-		self.url = base_url+quote_url
+		self.url = self.base_url+self.quote_url
 		self.response = requests.request("GET", self.url)
 		self.message = json.loads(self.response.text)
-		self.final_message = message['data']
+		self.final_message = self.message['data']
 		pairs = []
-		for symbol in final_message:
+		for symbol in self.final_message:
 			self.symbol = symbol["symbol"]
 			pairs.append(self.symbol)
 		return pairs
@@ -22,21 +22,21 @@ class assets :
 	async def get_pairs(self):
 		self.base_url = "https://bitmax.io"
 		self.quote_url = "/api/pro/v1/products"
-		self.url = base_url+quote_url
+		self.url = self.base_url+self.quote_url
 		self.response = requests.request("GET", self.url)
 		self.message = json.loads(self.response.text)
-		self.final_message = message['data']
+		self.final_message = self.message['data']
 		pairs = []
 		self.exchange = 'bitmax'
-		self.dsn = "postgres://postgres:kirahavethedeathnote123A@localhost:5432/"+exchange
+		dsn = "postgres://postgres:kirahavethedeathnote123A@localhost:5432/"+self.exchange
 		print('inserting pairs is in process ...')
 		for symbol in self.final_message:
 			self.symbol = symbol["symbol"]
-			pairs.appendself.(symbol)
-			self.conn = await asyncpg.connect(self.dsn)
-			self.INSERT_INTO_TABLE= 'INSERT INTO all_pairs (pair) VALUES ' +"("+"'"+str(symbol)+"'"+")"
-			await conn.execute(self.INSERT_INTO_TABLE)
-			await conn.close()
+			pairs.append(self.symbol)
+			self.conn = await asyncpg.connect(dsn)
+			self.INSERT_INTO_TABLE= 'INSERT INTO all_pairs (pair) VALUES ' +"("+"'"+str(self.symbol)+"'"+")"
+			await self.conn.execute(self.INSERT_INTO_TABLE)
+			await self.conn.close()
 		print('all pairs has been inserted successfully')
 		return pairs
 
